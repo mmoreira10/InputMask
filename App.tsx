9f4de4cebe278b6a0c0701a9f1b8f1fed41b879b
mmoreira10 <matheus_moreira_1095@hotmail.com>
2020-09-11 00:29:13 -0300

    Commit app InputMask

--- App.tsx
+++ App.tsx
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Input from './src/components/Input';
 
 export default function App() {
+
+  const [cep, setCep] = useState("");
+  const [phone, setPhone] = useState("");
+  const [currency, setCurrency] = useState("");
+
+  // function handleCustom(value: string) {
+  //   setCep(value);
+  // }
+
+  function handleSubmit() {
+    console.log(
+      cep,
+      phone,
+      currency
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Input
+        value={cep}
+        mask="cep"
+        maxLength={9}
+        placeholderTextColor="#000"
+        placeholder="Digite o CEP"
+        inputMaskChange={(text: string) => setCep(text)} />
+
+      <Input
+        value={phone}
+        mask="phone"
+        maxLength={15}
+        placeholderTextColor="#000"
+        placeholder="Digite o celular"
+        inputMaskChange={(text: string) => setPhone(text)} />
+
+      <Input
+        value={currency}
+        mask="currency"
+        placeholderTextColor="#000"
+        placeholder="Digite o valor"
+        inputMaskChange={(text: string) => setCurrency(text)} />
+
+      <Button title="Cadastrar" onPress={handleSubmit} />
     </View>
   );
 }
@@ -14,7 +55,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#719',
     alignItems: 'center',
     justifyContent: 'center',
   },
